36ce3901c1dc481a355b15cd5bb782839c2f269f
Dicle Bulut <diclebulut@Dicles-MacBook-Air.local>
2026-02-01 02:57:50 +0000

    added a more complex static pricing, added deep q learning model

--- src/modules/config.py
+++ src/modules/config.py
@@ -1,14 +1,8 @@
 # ============================================
-# Data Version
-# ============================================
-
-VERSION = 'V2' 
-# ============================================
 # Data Paths
 # ============================================
 
-DATA_PATH_V1 = 'data/uber_data.csv'
-DATA_PATH_V2 = 'data/fhvhv_tripdata_2021-01.parquet'
+DATA_PATH = 'data/fhvhv_tripdata_2021-01.parquet'
 TAXI_ZONE_PATH = 'data/taxi_zone_lookup.csv'
 WEATHER_NYC_PATH = 'data/nyc 2021-01-01 to 2021-12-31.csv'
 
@@ -16,57 +10,11 @@ WEATHER_NYC_PATH = 'data/nyc 2021-01-01 to 2021-12-31.csv'
 # Columns as features
 # ============================================
 
-CATEGORICAL_FEATURES_V1 = ['Location_Category', 'Customer_Loyalty_Status', 'Time_of_Booking', 'Vehicle_Type']
-NUMERICAL_FEATURES_V1 = ['Number_of_Riders', 'Number_of_Drivers', 'Number_of_Past_Rides', 'Average_Ratings', 'Expected_Ride_Duration']
-TARGET_COLUMN_V1 = 'Historical_Cost_of_Ride'
-
-CATEGORICAL_FEATURES_V2 = ['PULocationID', 'DOLocationID', 'shared_request_flag', 
+CATEGORICAL_FEATURES = ['PULocationID', 'DOLocationID', 'shared_request_flag', 
                         'shared_match_flag', 'out_of_base_dispatch_flag',
                         'time_of_day','preciptype'
                         ]
-NUMERICAL_FEATURES_V2 = ['trip_miles', 'trip_time', 'tips',
+NUMERICAL_FEATURES = ['trip_miles', 'trip_time', 'tips',
                        'wait', 'precip', 'day_of_month']
-TARGET_COLUMN_V2 = 'final_fare'
+TARGET_COLUMN = 'final_fare'
 
-# ============================================
-# Active (versioned) config
-# ============================================
-
-if VERSION == 'V1':
-    DATA_PATH = DATA_PATH_V1
-    CATEGORICAL_FEATURES = CATEGORICAL_FEATURES_V1
-    NUMERICAL_FEATURES = NUMERICAL_FEATURES_V1
-    TARGET_COLUMN = TARGET_COLUMN_V1
-elif VERSION == 'V2':
-    DATA_PATH = DATA_PATH_V2
-    CATEGORICAL_FEATURES = CATEGORICAL_FEATURES_V2
-    NUMERICAL_FEATURES = NUMERICAL_FEATURES_V2
-    TARGET_COLUMN = TARGET_COLUMN_V2
-else:
-    raise ValueError(f"Unsupported VERSION: {VERSION}")
-
-# Basic train test split parameters
-# ============================================
-
-TEST_SIZE = 0.2
-RANDOM_STATE = 42
-
-# ============================================
-# Gradient Descent parameters
-# ============================================
-
-LEARNING_RATE_GD = 1e-5
-MAX_ITERATIONS = 50
-COST = 0
-
-# ============================================
-# Reinforcement Learning parameters
-# ============================================
-
-N_PRICE_ACTIONS=20
-LEARNING_RATE_RL=0.1
-DISCOUNT=0.95
-EPSILON=1.0
-EPSILON_DECAY=0.995
-EPISODES=1000
-N_EVAL_EPISODES=50
--- walkthroughs/notes.md
+++ walkthroughs/notes.md
@@ -23,3 +23,18 @@ Uber profit measures (try separately with all 3)
 Steps taken to pick features
 - Variance Inflation Factor (VIF) analysis
 - Basic correlation matrix
+
+Model Plans
+passenger_model = uber_trips[['PULocationID', 'DOLocationID', 'trip_miles',
+                              'tips','shared_request_flag', 'shared_match_flag',
+                              'final_fare', 'wait', 'day_of_month',
+                            'time_of_day', 'precip', 'preciptype']]
+
+driver_model = uber_trips[['PULocationID', 'DOLocationID', 'trip_miles','out_of_base_dispatch_flag',
+                           'driver_final_pay', 'wait', 'day_of_month',
+                            'time_of_day', 'precip', 'preciptype']]
+
+uber_side_passenger_model = [['PULocationID', 'DOLocationID', 'trip_miles',
+                              'tips','shared_request_flag', 'shared_match_flag',
+                              'uber_profit', 'wait', 'day_of_month',
+                            'time_of_day', 'precip', 'preciptype']]
